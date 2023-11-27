91f20bdfd7a37aa98e3504ca47ca6df42381b6f7
Alexander <alex@alexvoiceover.com>
2023-11-27 20:17:25 +0000

    Fixed bot

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
 // Load environment variables from .env file
-require("dotenv").config()
+require('dotenv').config()
 
 // Import necessary classes from discord.js library
-const { Client, Events, GatewayIntentBits } = require("discord.js")
+const { Client, Events, GatewayIntentBits } = require('discord.js')
 
 // Create a new client instance
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.MessageContent,
+	],
 })
 
 client.once(Events.ClientReady, (createdClient) => {
-  console.log(`Ready! Logged in as ${createdClient.user.tag}`)
-  rl.prompt() // Initial prompt
+	console.log(`Ready! Logged in as ${createdClient.user.tag}`)
+	rl.prompt() // Initial prompt
 })
 
 client.on(Events.MessageCreate, async (message) => {
-  // Ignore messages from bots to prevent infinite loops
-  if (message.author.bot) return
+	// Ignore messages from bots to prevent infinite loops
+	if (message.author.bot) return
 
-  console.log() // Add a line break before the message
-  console.log(`Message from ${message.author.tag}: ${message.content}`)
-  rl.prompt() // Re-prompt after logging the message
+	console.log() // Add a line break before the message
+	console.log(`Message from ${message.author.tag}: ${message.content}`)
+	rl.prompt() // Re-prompt after logging the message
 })
 
-const readline = require("readline")
+const readline = require('readline')
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 })
 
-rl.setPrompt("Enter a message to send: ")
+rl.setPrompt('Enter a message to send: ')
 
-rl.on("line", (input) => {
-  // The channel ID where you want to send messages
-  const channelId = "1178786990196527184" // Replace with your actual channel ID
+rl.on('line', (input) => {
+	// The channel ID where you want to send messages
+	const channelId = '1178786990641131673' // Replace with your actual channel ID
 
-  // Fetch the channel from the client
-  const channel = client.channels.cache.get(channelId)
-  if (channel) {
-    // Send the message to the channel
-    channel
-      .send(input)
-      .then(() => {
-        rl.prompt() // Prompt for the next input
-      })
-      .catch(console.error)
-  } else {
-    console.log("Channel not found!")
-    rl.prompt()
-  }
+	// Fetch the channel from the client
+	const channel = client.channels.cache.get(channelId)
+	if (channel) {
+		// Send the message to the channel
+		channel
+			.send(input)
+			.then(() => {
+				rl.prompt() // Prompt for the next input
+			})
+			.catch(console.error)
+	} else {
+		console.log('Channel not found!')
+		rl.prompt()
+	}
 })
 
 // Log in to Discord with your client's token
