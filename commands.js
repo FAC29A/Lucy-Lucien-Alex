8af1e2ae666818eb256625e9be95a093b0d8edc5
Lucien Lin <luciensday@gmail.com>
2023-11-28 15:56:37 +0000

    add echo function

--- commands.js
+++ commands.js
@@ -1,25 +1,33 @@
 // Data structure were we store all the commands
 const commandActions = {
-	ping: (message) => sendMessage(message, 'Pong!'),
-	hello: (message) => sendMessage(message, 'Hi there!'),
-	randomJoke: (message) => randomJoke(message),
-	help: sendHelpMessage,
-	// Add more commands and actions here
-}
+  ping: (message) => sendMessage(message, "Pong!"),
+  hello: (message) => sendMessage(message, "Hi there!"),
+  randomJoke: (message) => randomJoke(message),
+  help: sendHelpMessage,
+  echo: echoMessage,
+  // Add more commands and actions here
+};
 
 function sendMessage(message, response) {
-	message.reply(response)
+  message.reply(response);
 }
 
 function sendHelpMessage(message) {
-	const helpText =
-		'Available commands:\n\t!hello: returns "Hi There"\n\t!ping: returns "Pong!"'
-	message.reply(helpText)
+  const helpText =
+    'Available commands:\n\t!hello: returns "Hi There"\n\t!ping: returns "Pong!"';
+  message.reply(helpText);
 }
 
 function randomJoke(message) {
-	message.reply()
+  message.reply();
 }
 
+function echoMessage(message) {
+  // The resason of slice(5) is because there are 5 character in !echo
+  const messageWithoutPrefix = message.content.slice(5).trim();
+  message.reply(messageWithoutPrefix);
 
-module.exports = commandActions
+  console.log(message);
+}
+
+module.exports = commandActions;
