358d292aa774a31ff90eb812972db818a8ad6850
Alexander <alex@alexvoiceover.com>
2023-11-28 21:22:48 +0000

    Fixedd use of chatGPT

--- commands.js
+++ commands.js
@@ -1,13 +1,12 @@
 const jokes = require('./jokes')
 const history = require('./history')
-const { Configuration, OpenAIApi } = require('openai')
+const OpenAIApi = require('openai')
 
 // Initialize OpenAI SDK with API key from .env file
-const openai = new OpenAIApi(
-	new Configuration({
-		apiKey: process.env.OPENAI_API_KEY,
-	})
-)
+
+const openai = new OpenAIApi({
+	apiKey: process.env.OPENAI_API_KEY,
+})
 
 // Data structure were we store all the commands
 const commandActions = {
@@ -58,16 +57,27 @@ async function chatGPT(message) {
 	// Extract the query from the message
 	const query = message.content.slice('!ask '.length).trim()
 
-	// Call the OpenAI API
+	// Call the OpenAI API for a chat completion
 	try {
-		const response = await openai.Completion.create({
-			model: 'text-davinci-003', // or any other model you prefer
-			prompt: query,
-			max_tokens: 150,
+		const completion = await openai.chat.completions.create({
+			model: 'gpt-3.5-turbo',
+			messages: [
+				{ role: 'system', content: 'You are a helpful assistant.' },
+				{ role: 'user', content: query },
+			],
 		})
 
+		console.log(completion.choices[0].message)
+
+		// Assuming the message object has a property 'content' which is the reply
+		const reply = completion.choices[0].message.content
+
 		// Send the response back to Discord
-		message.reply(response.choices[0].text.trim())
+		if (reply) {
+			message.reply(reply.trim())
+		} else {
+			message.reply("I didn't get a response. Please try again.")
+		}
 	} catch (error) {
 		console.error('Error calling OpenAI:', error)
 		message.reply(
