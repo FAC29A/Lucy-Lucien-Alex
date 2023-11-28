48028cdf4efe35e8322323a01ddf90b3cee7e720
Alexander <alex@alexvoiceover.com>
2023-11-28 20:41:02 +0000

    Added chatGPT

--- commands.js
+++ commands.js
@@ -1,5 +1,13 @@
 const jokes = require('./jokes')
 const history = require('./history')
+const { Configuration, OpenAIApi } = require('openai')
+
+// Initialize OpenAI SDK with API key from .env file
+const openai = new OpenAIApi(
+	new Configuration({
+		apiKey: process.env.OPENAI_API_KEY,
+	})
+)
 
 // Data structure were we store all the commands
 const commandActions = {
@@ -8,7 +16,7 @@ const commandActions = {
 	joke: (message) => randomJokes(message),
 	echo: (message) => echoMessage(message),
 	history: sendHistory,
-
+	ask: (message) => chatGPT(message),
 	help: sendHelpMessage,
 	// Add more commands and actions here
 }
@@ -44,6 +52,28 @@ function echoMessage(message) {
 	// The resason of slice(5) is because there are 5 character in !echo
 	const messageWithoutPrefix = message.content.slice(5).trim()
 	message.reply(messageWithoutPrefix)
+}
+
+async function chatGPT(message) {
+	// Extract the query from the message
+	const query = message.content.slice('!ask '.length).trim()
+
+	// Call the OpenAI API
+	try {
+		const response = await openai.Completion.create({
+			model: 'text-davinci-003', // or any other model you prefer
+			prompt: query,
+			max_tokens: 150,
+		})
+
+		// Send the response back to Discord
+		message.reply(response.choices[0].text.trim())
+	} catch (error) {
+		console.error('Error calling OpenAI:', error)
+		message.reply(
+			'Sorry, I encountered an error while processing your request.'
+		)
+	}
 }
 
 module.exports = commandActions
